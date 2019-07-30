4b925c5b7276c49b5d588c525cdaebd339d8fd51
Himanshu Raj <himanshu@websitepandas.com>
2019-07-31 04:36:29 +0530

    Added a redirection URI after logging in with google account.

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -5,9 +5,8 @@ const keys = require('./keys');
 passport.use(
     new GoogleStrategy({
         // Options for the google startegy
+        callbackURL: '/auth/google/redirect',
         clientID: keys.google.clientID,
         clientSecret: keys.google.clientSecret
-    }), () => {
-
-    }
+    }, () => {})
 );
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const passport = require('passport');
 
 // auth login
 router.get('/login', (req, res) => {
@@ -12,9 +13,17 @@ router.get('/logout', (req, res) => {
 });
 
 //auth with google
-router.get('/google', (req, res) => {
-    // Handle with passport.js
-    res.send("logging in with google");
+// router.get('/google', (req, res) => {
+//     // Handle with passport.js
+//     res.send("logging in with google");
+// });
+router.get('/google', passport.authenticate('google', {
+    scope: ['profile']
+}));
+
+// callback route for google to redirect to
+router.get('/google/redirect', (req, res) => {
+    res.send('You reached a callback URI');
 });
 
 module.exports = router;

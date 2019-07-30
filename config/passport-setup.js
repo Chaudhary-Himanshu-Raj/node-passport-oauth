d43064e1e43f399d49f13da5fb162ed30d643f70
Himanshu Raj <himanshu@websitepandas.com>
2019-07-31 03:54:29 +0530

    Added .gitignore file, with key file

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,9 +1,12 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
+const keys = require('./keys');
 
 passport.use(
     new GoogleStrategy({
         // Options for the google startegy
+        clientID: keys.google.clientID,
+        clientSecret: keys.google.clientSecret
     }), () => {
 
     }

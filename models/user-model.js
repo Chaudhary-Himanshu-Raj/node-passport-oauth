17d80d346befc53d43761405a4b98f76e3115659
Himanshu Raj <himanshu@websitepandas.com>
2019-07-31 05:51:42 +0530

    Added user model for persisting user data using schema.

---
+++ models/user-model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create a schema.
+const userSchema = new Schema({
+    username: String,
+    google_id: String
+});
+
+//Create an instance using schema
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;

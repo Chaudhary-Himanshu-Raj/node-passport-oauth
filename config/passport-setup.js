7f466d96f686f9ed50301e8dd7b3b6aa0c033d5d
Himanshu Raj <himanshu@websitepandas.com>
2019-07-31 06:38:50 +0530

    Check if a user exists. if not create one, else print existing user.

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -12,12 +12,22 @@ passport.use(
     }, (accessToken, refreshToken, profile, done) => {
         console.log("Passport callback is triggered here...");
         console.log(profile);
-        new User({
-                google_id: profile.id,
-                username: profile.displayName
-            }).save()
-            .then((newUser) => {
-                console.log('new user created: ', newUser);
-            });
+        // check if user already exists in our own db
+        User.findOne({ google_id: profile.id }).then((currentUser) => {
+            if (currentUser) {
+                // already have this user
+                console.log('user is: ', currentUser);
+                // do something
+            } else {
+                // if not, create user in our db
+                new User({
+                    google_id: profile.id,
+                    username: profile.displayName
+                }).save().then((newUser) => {
+                    console.log('created new user: ', newUser);
+                    // do something
+                });
+            }
+        });
     })
 );
